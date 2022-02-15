6bbd1655b1e48c5dbc5e6116c338991d522fa4a9
emiryucell <e.talhayucel@gmail.com>
2022-02-15 19:56:51 +0300

    basic graphics added.

--- core/src/com/emirtechs/birdescape/BirdEscape.java
+++ core/src/com/emirtechs/birdescape/BirdEscape.java
@@ -1,6 +1,7 @@
 package com.emirtechs.birdescape;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
@@ -9,16 +10,29 @@ public class BirdEscape extends ApplicationAdapter {
 
 
     SpriteBatch batch;
-
+    Texture bg;
+    Texture bird;
+    float birdX = 0;
+    float birdY = 0;
 
     @Override
     public void create() {
         batch = new SpriteBatch();
+        bg = new Texture("background.png");
+        bird = new Texture("bird.png");
+
+        birdX = Gdx.graphics.getWidth() / 4;
+        birdY = Gdx.graphics.getHeight() / 2;
 
     }
 
     @Override
     public void render() {
+        batch.begin();
+        batch.draw(bg, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        batch.draw(bird, birdX, birdY, Gdx.graphics.getWidth() / 15, Gdx.graphics.getHeight() / 10);
+
+        batch.end();
 
     }
 
