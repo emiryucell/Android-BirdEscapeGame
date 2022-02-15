b7b556c626bf68ebe7d3067b1a6d0df2e3760ab0
emiryucell <e.talhayucel@gmail.com>
2022-02-15 19:03:49 +0300

    project beginning

---
+++ android/src/com/emirtechs/birdescape/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.emirtechs.birdescape;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.emirtechs.birdescape.BirdEscape;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new BirdEscape(), config);
+	}
+}
